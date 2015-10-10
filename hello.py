acf68bcaf16aa96dcf9f0625659fe3de88a81537
xoSauce <sergiu.ferentz@gmail.com>
2015-10-10 17:10:29 +0100

    Adding sessions

--- hello.py
+++ hello.py
@@ -2,10 +2,23 @@ import os
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
+
+def sumSessionCounter():
+	try:
+		session['counter'] += 1
+	except KeyError:
+		session['counter'] = 1
+
+def generateSessionKey():
+	import string
+	import random
+	return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
 
 @app.route('/')
 def hello():
-    return "hello world"
+	sumSessionCounter()
+    return generateSessionKey()
 
 @app.route('/stuff')
 def hey():
