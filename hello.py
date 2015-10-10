5061f24c47df4e7e2ab8580d10c58ca17095e411
xoSauce <sergiu.ferentz@gmail.com>
2015-10-10 18:28:58 +0100

    Users

--- hello.py
+++ hello.py
@@ -1,29 +1,32 @@
 import os
-from flask import Flask
+from flask import Flask, request, redirect, url_for
+from flask import make_response
 from flask import render_template
 app = Flask(__name__)
 app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
 
-def sumSessionCounter():
-	try:
-		session['counter'] += 1
-	except KeyError:
-		session['counter'] = 1
-
 def generateSessionKey():
 	import string
 	import random
-	return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
+	return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for i in range(12))
+
+@app.route('/set', methods=['GET','POST'])
+def setSession():
+	response = redirect('/')
+	response.set_cookie('myapp_user', generateSessionKey())
+	return response	
 
 @app.route('/')
-def hello():
-	sumSessionCounter()
-    return generateSessionKey()
+def index():
+	user_id = request.cookies.get('myapp_user')
+	if user_id:	
+		return "Welcome - Already set" + user_id
+	else:
+		return redirect('/set')
 
-@app.route('/stuff')
+@app.route('/play')
 def hey():
-    myVar = 'xoSauce'
-    return render_template('get_data.html', myVar=myVar)
+    return render_template('get_data.html')
 
 if __name__ == '__main__':
-    app.run(threaded=True)
+    app.run(threaded=True, debug=True)
