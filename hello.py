c65807b42adbd1d316b83faa8e5758407afbfd4c
xoSauce <sergiu.ferentz@gmail.com>
2015-10-10 15:17:24 +0100

    html

--- hello.py
+++ hello.py
@@ -1,10 +1,16 @@
 import os
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-        return 'Hello World!'
+    return "hello world"
+
+@app.route('/stuff')
+def hey():
+    myVar = 'xoSauce'
+    return render_template('get_data.html', myVar=myVar)
+
 if __name__ == '__main__':
     app.run(threaded=True)
